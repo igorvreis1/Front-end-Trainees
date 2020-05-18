ab44f2e7f03a84c632f15b2f44894863b2864c99
Pedro Daniel Camargos Soares <48827474+PedroDCS@users.noreply.github.com>
2020-05-18 19:14:50 -0300

    versão Alpha, só pra teste
    Ainda falta melhorar o visual, e também adicionar o resto dos produtos

---
+++ Teste-Produtos-Alpha1/produtos.js
@@ -0,0 +1,6 @@
+ var produto1={"nome": "Máscara De Pano Reutilizável Dupla Camada - 1 Pçs", "preco":" 99,99","quantidade":"45"};
+				
+				function preencher(){
+					document.getElementById("nomeprod") .innerHTML=produto1.nome;
+					document.getElementById("precoprod") .innerHTML=produto1.preco;
+				}
