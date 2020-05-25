1b5760dd8826d710594e7463aaf832eb36a8d3fa
Pedro <31280460+iGameKiller@users.noreply.github.com>
2020-05-25 03:23:36 -0300

    Add files via upload

---
+++ Update 25-05 3h20/produtos.js
@@ -0,0 +1,39 @@
+var produto1={"tipo":"mascara", "nome": "Máscara De Pano Reutilizável Dupla Camada - 1 Pç", "preco":"R$ 2,99"};
+var produto2={"tipo":"mascara", "nome": "Máscara De Pano Reutilizável Dupla Camada - 5 Pçs", "preco":"R$ 14,99"};
+var produto3={"tipo":"mascara", "nome": "Máscara De Pano Reutilizável Dupla Camada - 10 Pçs", "preco":"R$ 28,99"};
+var produto4={"tipo":"mascara", "nome": "Máscara De Pano Reutilizável Dupla Camada - 50 Pçs", "preco":"R$ 139,99"};
+
+var produto5={"tipo":"luva", "nome": "Luva de Látex Descartável - 1 Pç", "preco":"R$ 0,99"};
+var produto6={"tipo":"luva", "nome": "Luva de Látex Descartável - 5 Pçs", "preco":"R$ 4,49"};
+var produto7={"tipo":"luva", "nome": "Luva de Látex Descartável - 10 Pçs", "preco":"R$ 8,99"};
+var produto8={"tipo":"luva", "nome": "Luva de Látex Descartável - 100 Pçs", "preco":"R$ 38,83"};
+
+var produto9={"tipo":"alcoolgel", "nome": "Alcool em Gel SepPro Gel 1L - 1 Unidade", "preco":"R$ 11,99"};
+var produto10={"tipo":"alcoolgel", "nome": "Alcool em Gel SepPro Gel 1L - 5 Unidades", "preco":"R$ 58,99"};
+var produto11={"tipo":"alcoolgel", "nome": "Alcool em Gel SepPro Gel 1L - 10 Unidades", "preco":"R$ 117,99"};
+var produto12={"tipo":"alcoolgel", "nome": "Alcool em Gel SepPro Gel 1L - 100 Unidades", "preco":"R$ 1190,00"};
+
+var produto13={"tipo":"alcool", "nome": "Acool 70% 1L - 1 Unidade", "preco":"R$ 8,99"};
+var produto14={"tipo":"alcool", "nome": "Acool 70% 1L - 5 Unidades", "preco":"R$ 43,99"};
+var produto15={"tipo":"alcool", "nome": "Acool 70% 1L - 10 Unidades", "preco":"R$ 87,99"};
+var produto16={"tipo":"alcool", "nome": "Acool 70% 1L - 100 Unidades", "preco":"R$ 890,00"};
+
+var produto17={"tipo":"mascara", "nome": "Máscara De Pano Reutilizável Dupla Camada - 1 Pçs", "preco":"R$ 99,99"};
+var produto18={"tipo":"mascara", "nome": "Máscara De Pano Reutilizável Dupla Camada - 1 Pçs", "preco":"R$ 99,99"};
+var produto19={"tipo":"mascara", "nome": "Máscara De Pano Reutilizável Dupla Camada - 1 Pçs", "preco":"R$ 99,99"};
+var produto20={"tipo":"mascara", "nome": "Máscara De Pano Reutilizável Dupla Camada - 1 Pçs", "preco":"R$ 99,99"};
+var produto21={"tipo":"mascara", "nome": "Máscara De Pano Reutilizável Dupla Camada - 1 Pçs", "preco":"R$ 99,99"};
+var produto22={"tipo":"mascara", "nome": "Máscara De Pano Reutilizável Dupla Camada - 1 Pçs", "preco":"R$ 99,99"};
+var produto23={"tipo":"mascara", "nome": "Máscara De Pano Reutilizável Dupla Camada - 1 Pçs", "preco":"R$ 99,99"};
+var produto24={"tipo":"mascara", "nome": "Máscara De Pano Reutilizável Dupla Camada - 1 Pçs", "preco":"R$ 99,99"};
+var produto25={"tipo":"mascara", "nome": "Máscara De Pano Reutilizável Dupla Camada - 1 Pçs", "preco":"R$ 99,99"};
+var produto26={"tipo":"mascara", "nome": "Máscara De Pano Reutilizável Dupla Camada - 1 Pçs", "preco":"R$ 99,99"};
+var produto27={"tipo":"mascara", "nome": "Máscara De Pano Reutilizável Dupla Camada - 1 Pçs", "preco":"R$ 99,99"};
+var produto28={"tipo":"mascara", "nome": "Máscara De Pano Reutilizável Dupla Camada - 1 Pçs", "preco":"R$ 99,99"};
+var produto29={"tipo":"mascara", "nome": "Máscara De Pano Reutilizável Dupla Camada - 1 Pçs", "preco":"R$ 99,99"};
+var produto30={"tipo":"mascara", "nome": "Máscara De Pano Reutilizável Dupla Camada - 1 Pçs", "preco":"R$ 99,99"};
+var produto31={"tipo":"mascara", "nome": "Máscara De Pano Reutilizável Dupla Camada - 1 Pçs", "preco":"R$ 99,99"};
+var produto32={"tipo":"mascara", "nome": "Máscara De Pano Reutilizável Dupla Camada - 1 Pçs", "preco":"R$ 99,99"};
+var produto33={"tipo":"mascara", "nome": "Máscara De Pano Reutilizável Dupla Camada - 1 Pçs", "preco":"R$ 99,99"};
+var produto34={"tipo":"mascara", "nome": "Máscara De Pano Reutilizável Dupla Camada - 1 Pçs", "preco":"R$ 99,99"};
+var produto35={"tipo":"mascara", "nome": "Máscara De Pano Reutilizável Dupla Camada - 1 Pçs", "preco":"R$ 99,99"};
