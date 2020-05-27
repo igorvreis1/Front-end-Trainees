209069298b8dfb76c42b5a2ee2fcb8e7f164399f
Pedro Daniel Camargos Soares <48827474+PedroDCS@users.noreply.github.com>
2020-05-27 16:24:13 -0300

    Atualizando e Organizando

---
+++ Main/produtos.js
@@ -0,0 +1,25 @@
+var produto1={"tipo":"mascara", "nome": "Máscara de Pano Reutilizável Dupla Camada - 1 Unidade", "preco":"R$ 2,99"};
+var produto2={"tipo":"mascara", "nome": "Máscara de Pano Reutilizável Dupla Camada - kit 5 Unidades", "preco":"R$ 14,49"};
+var produto3={"tipo":"mascara", "nome": "Máscara de Pano Reutilizável Dupla Camada - kit 10 Unidades", "preco":"R$ 28,99"};
+var produto4={"tipo":"mascara", "nome": "Máscara de Pano Reutilizável Dupla Camada - kit 50 Unidades", "preco":"R$ 139,99"};
+
+var produto5={"tipo":"luva", "nome": "Luva De Látex Com Pó P/ Procedimento - Uso Médico - 1 Pacote com 100 unidades", "preco":"R$ 42,90"};
+var produto6={"tipo":"luva", "nome": "Luva De Látex Com Pó P/ Procedimento - Uso Médico - 2 Pacote com 100 unidades", "preco":"R$ 85,00"};
+var produto7={"tipo":"luva", "nome": "Luva De Látex Com Pó P/ Procedimento - Uso Médico - 3 Pacote com 100 unidades", "preco":"R$ 128,00"};
+var produto8={"tipo":"luva", "nome": "Luva De Látex Com Pó P/ Procedimento - Uso Médico - 4 Pacote com 100 unidades", "preco":"R$ 170,00"};
+
+var produto9={"tipo":"alcoolgel", "nome": "ÁLCOOL GEL ASSEPTGEL 52G DISPLAY COM 1UND", "preco":"R$ 5,85"};
+var produto10={"tipo":"alcoolgel", "nome": "ÁLCOOL GEL ASSEPTGEL 52G DISPLAY COM 12UND", "preco":"R$ 69,90"};
+var produto11={"tipo":"alcoolgel", "nome": "ÁLCOOL GEL ASSEPTGEL 52G DISPLAY COM 24UND", "preco":"R$ 139,50"};
+var produto12={"tipo":"alcoolgel", "nome": "ÁLCOOL GEL ASSEPTGEL 52G DISPLAY COM 48UND", "preco":"R$ 279,60"};
+
+var produto13={"tipo":"alcool", "nome": "Álcool 70% Etílico Líquido Tupi 1lt ","preco":"R$ 8,99"};
+var produto14={"tipo":"alcool", "nome": "Álcool 70% Etílico Líquido Tupi 1lt Cx C/ 12 Und", "preco":"R$ 107,88"};
+var produto15={"tipo":"alcool", "nome": "Álcool 70% Etílico Líquido Tupi 1lt Cx C/ 24 Und", "preco":"R$ 215,76"};
+var produto16={"tipo":"alcool", "nome": "Kit 3 Unidades de Álcool Líquido 70% Farmax 70º - 500ml", "preco":"R$ 23,72"};
+
+
+var descmasc="Máscara fabricada de acordo com as recomendações da OMS, feita em dupla camada de tecido, garantindo proteção, qualidade e conforto da respiração. Ideal para isolar vírus, poeira, germes, fumaça, poluição, etc. Possui corte ergonômico para se ajustar perfeitamente ao rosto, se adapta em qualquer formato de rosto. Produto lavável. Tecido: Tricoline e Elastano. Tipo de fixação: Elásticos que prendem atrás das orelhas (super confortável)<br> Recomendações de lavagem:<br> 1 - Lavar com água fria ou morna<br> 2 - Lavar com sabão neutro<br>"
+var descluva="Luvas descartáveis, sua qualidade praticamente elimina o risco de rompimento, minimiza o risco de contaminação por vírus e bactérias. São impermeáveis e econômicas. Possuem um toque de seda confortável	que proporciona alta sensibilidade ao tato."
+var descalcoolgel="O Álcool Gel 70º gl higienizante para as mãos da Body Blue mata 99,99% dos germes causadores de doenças, tem secagem rápida, dispensa o uso de água ou toalhas. Possui ação hidratante, rápida absorção e ação antisséptica.<br>A mão é a principal via de transmissão de micro-organismos, já que estão em constante contato com superfícies contaminadas, por isso a higienização adequada é a medida isolada individual mais eficaz para prevenir a transmissão de doenças para o ambiente e para as pessoas.<br>- Ação Hidratante<br>- Rápida absorção<br>- Ação antisséptica<br>- Mata 99,99% dos germes causadores de doença<br>- Previne a transmissão de vírus e bactérias<br>- Mãos protegidas e higienizadas<br>"
+var descalcool="Álcool Etílico Hidratado 70% INPM 5 litros<br>Nova embalagem com segurança !!!<br>Ideal para ser usado em:<br>- Proteção contra vírus e doenças;<br>- Hospitais;<br>- Clinicas;<br>- Escritórios;<br>- Cozinhas de Restaurante;<br>- Banheiros;<br>- Academias;<br>"
